21260545152b13491977ac1beeea3c301dd09bb8
ARTHUR IANTAS STELZNER <arthur_stelzner@estudante.sesisenai.org.br>
2026-08-11 15:57:40 -0300

    Começo das estilizações

--- view/js/index.js
+++ view/js/index.js
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 tarefas.forEach(tarefa => {
                     const card = document.createElement('div');
                     card.classList.add('card');
+                    card.
                     card.innerHTML = `
                         <h3>${tarefa.nome}</h3>
                         <span>${tarefa.descricao}</span>
